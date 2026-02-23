d16a8c0db70cf9abdd1e253cd94c0378d387282a
Zhang_newpc <zhangshibf@gmail.com>
2026-02-23 17:55:27 +0100

    .

---
+++ src/settings.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+ROOT_DIR = Path(__file__).parent.parent
+BENCHMAR_DIR = ROOT_DIR / "benchmark"
+OUTPUT_DIR = ROOT_DIR / "output"
+RESULTS_DIR = ROOT_DIR / "eval_results"
