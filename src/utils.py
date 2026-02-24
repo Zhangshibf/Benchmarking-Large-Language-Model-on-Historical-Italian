05616b785afef747b920423abeee71d333fd6df8
Zhang_newpc <zhangshibf@gmail.com>
2026-02-24 09:42:08 +0100

    .

--- src/utils.py
+++ src/utils.py
@@ -18,6 +18,22 @@ class BenchmarkInstance1:
     gold: str
     gold_index: int
 
+@dataclass
+class BenchmarkInstanceRanking:
+    id: int
+    snippets: List[str]
+    order: list[int]
+
+@dataclass
+class BenchmarkInstanceNERLetter:
+    dataset:str
+    id: str
+    text:str
+    entities:list[[str]]
+    entites_type:list[[str]]
+
+
+
 class BaseBenchmarkLoader(ABC):
     def __init__(self, benchmark_dir: Path):
         self.benchmark_dir = benchmark_dir
@@ -80,24 +96,81 @@ class Tier2Loader(BaseBenchmarkLoader):
                 gold_index=gold_index,
             )
 
+class Tier3Loader(BaseBenchmarkLoader):
+    def __init__(self, benchmark_dir: Path):
+        super().__init__(benchmark_dir)
+        self.bellini_path = self.benchmark_dir / "tier3" / "bellini.json"
+        self.classense_path = self.benchmark_dir / "tier3" / "classense.json"
+        self.paths = [self.bellini_path,self.classense_path]
+    def __iter__(self):
+        for p in self.paths:
+            with open(p,"r",encoding='utf-8') as f:
+                dataset = json.load(f)
+                dataset_name = "bellini" if "bellini" in p else "classense"
+                for letter in dataset:
+                    id = letter['letter_id']
+                    text=letter['text']
+                    entites = [i['text'] for i in letter['entities']]
+                    entites_type = [i['type'] for i in letter['entities']]
 
-class Tier5Loader(BaseBenchmarkLoader):
+                    yield BenchmarkInstanceNERLetter(dataset=dataset_name,id=id,text = text,entities=entites,entites_type=entites_type)
+
+
+class Tier5LoaderAuthorship(BaseBenchmarkLoader):
     def __init__(self, benchmark_dir: Path):
         super().__init__(benchmark_dir)
         self.narrativa_authorship = self.benchmark_dir / "tier5" / "narrativa_authorship.json"
         self.poesia_authorship = self.benchmark_dir / "tier5" / "poesia_authorship.json"
-        self.narrativa_ranking = self.benchmark_dir / "tier5" / "narrativa_temporal_ranking.json"
-        self.poesia_ranking = self.benchmark_dir / "tier5" / "poesia_temporal_ranking.json"
-        self.f_paths = [self.narrativa_authorship,self.poesia_authorship,self.narrativa_ranking,self.poesia_ranking]
+
+        self.f_paths = [self.narrativa_authorship,self.poesia_authorship]
     def __iter__(self):
         data=[]
         for f_path in self.f_paths:
             with open(f_path, "r", encoding="utf-8") as f:
                 data.append(json.load(f_path))
         for dataset in data:
-            task = dataset["file_info"]["type"]
             instances = dataset["tasks"]
-            if task=="ranking":
-                for instance in instances:
-                    snippets = [i['text'] for i in instance["snippets"]]
-                    years = [i['year'] for i in instance["snippets"]]
+
+            for instance in instances:
+                id = instance['task_id']
+                snippets = [i['text'] for i in instance["snippets"]]
+                gold_index = instance['solution']
+
+                yield BenchmarkInstance1(
+                    id=id,
+                    target="",
+                    context="",
+                    candidates=snippets,
+                    gold="",
+                    gold_index=gold_index,
+                )
+
+
+class Tier5LoaderRanking(BaseBenchmarkLoader):
+    def __init__(self, benchmark_dir: Path):
+        super().__init__(benchmark_dir)
+        self.narrativa_ranking = self.benchmark_dir / "tier5" / "narrativa_temporal_ranking.json"
+        self.poesia_ranking = self.benchmark_dir / "tier5" / "poesia_temporal_ranking.json"
+        self.f_paths = [self.narrativa_ranking,self.poesia_ranking]
+    def __iter__(self):
+        data=[]
+        for f_path in self.f_paths:
+            with open(f_path, "r", encoding="utf-8") as f:
+                data.append(json.load(f_path))
+        for dataset in data:
+
+            instances = dataset["tasks"]
+
+            for instance in instances:
+                id = instance['task_id']
+                snippets = [i['text'] for i in instance["snippets"]]
+                years = [i['year'] for i in instance["snippets"]]
+                years = [int(i) for i in years]
+                c = list(zip(snippets, years))
+                random.shuffle(c)
+                snippets, years = zip(*c)
+                sorted_year = sorted(years)
+                years_order = [sorted_year.index(i) for i in years]
+
+                yield BenchmarkInstanceRanking(id=id,snippets=snippets,order=years_order)
+
